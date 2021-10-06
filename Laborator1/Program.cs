eeab356d096a507b0436d127fb8568b592b1c1ad
Ciceu Tiberiu <45262167+ciceutiberiu@users.noreply.github.com>
2021-10-06 14:19:24 +0300

    Add project files.

---
+++ Laborator1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laborator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Laborator 1, Medii de programare si dezvoltare");
+        }
+    }
+}
